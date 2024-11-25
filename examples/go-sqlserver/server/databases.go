1eb572b86be47f055cc8c953436f590f3dd1ac7c
Satoshi Konno <skonno@cybergarage.org>
2024-11-25 19:37:18 +0900

    Add server.Databases

---
+++ examples/go-sqlserver/server/databases.go
@@ -0,0 +1,60 @@
+// Copyright (C) 2024 The go-sqlserver Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package server
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/cybergarage/go-sqlparser/sql/errors"
+)
+
+// Databases represents a collection of databases.
+type Databases struct {
+	sync.Map
+}
+
+// NewDatabases returns a databases instance.
+func NewDatabases() Databases {
+	return Databases{
+		sync.Map{},
+	}
+}
+
+// AddDatabase adds a specified database.
+func (dbs Databases) AddDatabase(db *Database) error {
+	dbName := db.Name()
+	if _, ok := dbs.Load(dbName); ok {
+		return fmt.Errorf("database %s already %w", dbName, errors.ErrExist)
+	}
+	dbs.Store(dbName, db)
+	return nil
+}
+
+// DropDatabase remove the specified database.
+func (dbs Databases) DropDatabase(db *Database) error {
+	name := db.Name()
+	dbs.Delete(name)
+	return nil
+}
+
+// LookupDatabase returns a database with the specified name.
+func (dbs Databases) LookupDatabase(name string) (*Database, bool) {
+	db, ok := dbs.Load(name)
+	if !ok {
+		return nil, false
+	}
+	return db.(*Database), true
+}
