6901ea18544ca1b3aab6d0b26d0cc9befc66c16e
Satoshi Konno <skonno@cybergarage.org>
2024-11-25 19:37:01 +0900

    Add store.Database

---
+++ examples/go-sqlserver/server/database.go
@@ -0,0 +1,45 @@
+// Copyright (C) 2024 The go-sqlserver Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package server
+
+import (
+	dbsql "database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// Database represents a destination or source database of query.
+type Database struct {
+	name string
+	*dbsql.DB
+}
+
+// NewDatabaseWithName returns a new database with the specified string.
+func NewDatabaseWithName(name string) (*Database, error) {
+	var err error
+	db := &Database{
+		name: name,
+	}
+	db.DB, err = dbsql.Open("sqlite3", ":memory:")
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+// Name returns the database name.
+func (db *Database) Name() string {
+	return db.name
+}
